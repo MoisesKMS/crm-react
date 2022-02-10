99ee328d581f4a82b2925e352417ebe6cddb813c
Moises Jimenez <MoisesJimenez@Yandex.com>
2022-02-09 19:03:41 -0600

    1.3.2
    -Eliminar Cliente

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -24,6 +24,24 @@ function Inicio() {
 
     }, [])
 
+    const handleEliminar = async id => {
+        const confirmar = confirm('¿Deseas eliminar este cliente?');
+        if (confirmar) {
+            try {
+                const url = `http://localhost:4000/clientes/${id}`;
+                const respuesta = await fetch(url, {
+                    method: 'DELETE'
+                })
+                await respuesta.json();
+
+                const arrayClientes = clientes.filter(cliente => cliente.id !== id);
+                setClientes(arrayClientes);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+    }
+
     return (
         <>
             <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
@@ -44,6 +62,7 @@ function Inicio() {
                         <Cliente
                             key={cliente.id}
                             cliente={cliente}
+                            handleEliminar={handleEliminar}
                         />
                     ))}
                 </tbody>
